8cb8e568f71d63f2ac36968b302d01fc1fcedd94
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2024-02-13 19:29:12 +0100

    chore: modify astro and tailwind config to accept new styles and fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   output: "hybrid",
   adapter: vercel(),
-  integrations: [tailwind()]
+  integrations: [tailwind({
+      applyBaseStyles: false
+  })]
 });
