8fa0f0adf0d2e797d86c8583797127929a3cabc3
Tanet Trimas <42898343+tanettrimas@users.noreply.github.com>
2024-02-20 20:05:20 +0100

    style: clamp font sizes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,17 +2,28 @@ import defaultTheme from "tailwindcss/defaultTheme"
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-            fontFamily: {
-                sans: ["JosefinSans-Light", "JosefinSans-SemiBold", "LimeLight-Regular", ...defaultTheme.fontFamily.sans]
-            },
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        fontFamily: {
+            sans: ["JosefinSans-Light", "JosefinSans-SemiBold", "LimeLight-Regular", ...defaultTheme.fontFamily.sans]
+        },
+        fontSize: {
+            xs: "var(--step--2)",
+            sm: "var(--step--1)",
+            base: "var(--step-0)",
+            lg: "var(--step-1)",
+            xl: "var(--step-2)",
+            "2xl": "var(--step-3)",
+            "3xl": "var(--step-4)",
+            "4xl": "var(--step-5)",
+            "5xl": "var(--step-6)",
+        },
+        extend: {
             colors: {
                 'darkgray': '#1a1a1a',
                 'turquoise': '#00f3d2'
-            }
-        },
-	},
-	plugins: [],
+            },
+        }
+    },
+    plugins: [],
 }
