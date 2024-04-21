1b12fe160b4ccb654e5b86e6bbc5e79a69b3d6d6
Ellaoui <abab010@kristiania.student.no>
2024-04-21 21:26:03 +0200

    Testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  adapter: vercel(),
-  integrations: [tailwind({
-      applyBaseStyles: false
-  })]
+    output: "hybrid",
+    adapter: vercel(),
+    integrations: [tailwind({
+        applyBaseStyles: false
+    })]
 });
