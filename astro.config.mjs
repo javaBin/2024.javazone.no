3c22e5e1a4cc35b53aadbc640820e83303ecdfdf
carolinevannebo <58633333+carolinevannebo@users.noreply.github.com>
2024-04-30 20:31:46 +0200

    edge middleware

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
+//import vercelStatic from '@astrojs/vercel/static';
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
@@ -7,7 +8,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: vercel(),
+  adapter: vercel({
+      edgeMiddleware: true,
+  }),
   integrations: [tailwind({
     applyBaseStyles: false
   }), react()]
