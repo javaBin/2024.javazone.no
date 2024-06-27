09af853f585d75899a9c7c315d9518046d626963
HaaVad <h.vadstein.k@gmail.com>
2024-06-27 15:31:30 +0200

    Update fetchProgram.ts

--- src/pages/program/fetchProgram.ts
+++ src/pages/program/fetchProgram.ts
@@ -1,32 +1,14 @@
 import type { Program } from "types/program"
 
-interface NextRevalidateConfig {
-  revalidate?: false | number
-  tags: string[] | undefined
+export async function fetchProgram() {
+  return await fetch('https://sleepingpill.javazone.no/public/allSessions/javazone_2024')
+    .then<Program>((res) => res.json());
 }
 
-export async function fetchProgram(
-  {
-    revalidateConfig,
-  }: {
-    revalidateConfig?: NextRevalidateConfig
-  } = {
-    revalidateConfig: {
-      tags: undefined,
-      revalidate: CACHE_TIME,
-    },
-  },
-) {
-  return await fetch('https://sleepingpill.javazone.no/public/allSessions/javazone_2024', {
-  }).then<Program>((res) => res.json())
-}
-
-const CACHE_TIME = 60 * 15 
 
 export const fetchIndividualProgram = (
   id?: string,
-  config: NextRevalidateConfig = { revalidate: CACHE_TIME, tags: undefined },
 ) =>
-  fetchProgram({ revalidateConfig: config }).then((program) =>
+  fetchProgram().then((program) =>
     program.sessions.find((session) => session.id === id),
   )
