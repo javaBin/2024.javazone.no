4150c429b842df94500a56834e14fd1a482a502d
Tanet Trimas <tanet.trimas@ruter.no>
2024-08-06 14:26:44 +0200

    feat: final adjustments on program page for now

--- src/utils/dateformat.ts
+++ src/utils/dateformat.ts
@@ -3,9 +3,14 @@ export const dayAndMonthFormat = Intl.DateTimeFormat("en", {
     month: "long"
 })
 
-export const dayAndTimeFormat = Intl.DateTimeFormat("en", {
+export const dayAndTimeFormatWithMonth = Intl.DateTimeFormat("en", {
     month: "long",
     day: "numeric",
+    hour: "numeric",
+    minute: "2-digit"
+})
+
+export const dayAndTimeFormat = Intl.DateTimeFormat("en", {
     hour: "numeric",
     minute: "2-digit"
 })
