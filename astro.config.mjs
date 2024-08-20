821a9457aa34f75723d4c7fb8f92af9bef9b8daf
Tanet Trimas <tanet.trimas@ruter.no>
2024-08-20 19:30:29 +0200

    feat: enable react components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
   integrations: [tailwind({
-      applyBaseStyles: false
-  })]
+    applyBaseStyles: false
+  }), react()]
 });
