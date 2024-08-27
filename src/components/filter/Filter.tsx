2d3c31e507c9b12c858ec352c0359766730535c7
carolinevannebo <58633333+carolinevannebo@users.noreply.github.com>
2024-08-27 19:58:51 +0200

    styling changes

--- src/components/filter/Filter.tsx
+++ src/components/filter/Filter.tsx
@@ -1,3 +1,4 @@
+import './filter.css';
 export type FilterType = "LIVE" | "FULL_PROGRAM" | "FAVORITES";
 
 interface Props {
@@ -8,7 +9,7 @@ interface Props {
 const Button = ({ children, onClick, current }: any) => (
     <button
         className={`${
-            current ? "bg-white text-darkgray border-2 active:bg-white py-6 px-6 sm:px-10 no-underline rounded-md hover:bg-white hover:border-2 border-darkgray hover:text-darkgray max-sm:text-lg whitespace-nowrap mb-2" : "bg-darkgreen active:bg-white py-6 px-6 sm:px-10 text-white no-underline rounded-md hover:bg-white hover:border-2 border-darkgray hover:text-darkgray max-sm:text-lg whitespace-nowrap mb-2"
+            current ? "bg-white text-darkgray border-2 active:bg-white py-6 px-6 sm:px-10 no-underline rounded-md hover:bg-white border-darkgray hover:text-darkgray max-sm:text-lg whitespace-nowrap mb-2" : "bg-darkgreen active:bg-white py-6 px-6 sm:px-10 text-white no-underline rounded-md border-2 border-transparent hover:bg-white hover:border-darkgray hover:text-darkgray max-sm:text-lg whitespace-nowrap mb-2"
         } p-2 rounded-lg`}
         onClick={onClick}
     >
@@ -17,15 +18,23 @@ const Button = ({ children, onClick, current }: any) => (
 );
 
 export const Filter = ({ filter, onChange }: Props) => (
-    <div className="bg-white border-black border-2 rounded-xl p-4 flex">
+    <div className="p-4 flex">
         <div className="mx-auto gap-10 flex flex-col md:flex-row">
+            <img
+                src="/images/characters/jz24-disc.svg"
+                alt="Retro disc character"
+                className="h-[80px] render-img"
+            />
             <Button
                 onClick={() => onChange("FULL_PROGRAM")}
                 current={filter === "FULL_PROGRAM"}
             >
                 Full program
             </Button>
-            <Button onClick={() => onChange("LIVE")} current={filter === "LIVE"}>
+            <Button
+                onClick={() => onChange("LIVE")}
+                current={filter === "LIVE"}
+            >
                 Live-mode
             </Button>
             <Button
@@ -34,7 +43,11 @@ export const Filter = ({ filter, onChange }: Props) => (
             >
                 My favorites
             </Button>
+            <img
+                src={`/images/characters/jz24-mc.svg`}
+                alt="Retro disc character"
+                className="h-[120px] -mt-[40px] render-img"
+            />
         </div>
-
     </div>
 );
