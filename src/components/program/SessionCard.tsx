6f56732b371f8d53901c5f1365c2b424610baf5d
Tanet Trimas <tanet.trimas@ruter.no>
2024-08-28 14:17:13 +0200

    fix: make most of the card clickable

--- src/components/program/SessionCard.tsx
+++ src/components/program/SessionCard.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import type { Session } from "../../types/program.ts";
-import {dayAndTimeFormat, dayAndTimeFormatWithMonth} from "../../utils/dateformat.ts";
+import { dayAndTimeFormatWithMonth } from "../../utils/dateformat.ts";
 import { useEffect, useState } from "react";
 
 interface Props {
@@ -42,38 +42,48 @@ export const SessionCard = ({ session, clicked, setClicked }: Props) => {
         setClicked((prev) => !prev);
     };
     return (
-        <div className="bg-white relative p-3 border-black border-2 rounded-xl hover:scale-105 transition ease-in-out delay-150 duration-200">
-            {session.room && session.startTime && (
-                <div className="flex justify-between pb-1 md:pb-2">
-                    <p className="my-0 md:my-1 text-sm md:text-base">
-                        {session.room} -{" "}
-                        {dayAndTimeFormatWithMonth.format(new Date(session.startTime))} - {session.format}
-                    </p>
-                    <button
-                        className="text-4xl hover:opacity-60"
-                        data-session-id={`${session.id}`}
-                        title={isFavorite ? "Remove from favorites" : "Add to favorites"}
-                        onClick={handleFavoriteClick}
-                    >
-                        {isFavorite ? "★" : "☆"}
-                    </button>
-                </div>
-            )}
-            <a href={`/program/${session.id}`} className="no-underline text-inherit">
+        <div className="flex justify-between bg-white relative p-3 border-black border-2 rounded-xl hover:scale-105 transition ease-in-out delay-150 duration-200">
+            <a
+                href={`/program/${session.id}`}
+                className="no-underline text-inherit"
+            >
+                {session.room && session.startTime && (
+                    <div className="flex justify-between pb-1 md:pb-2">
+                        <p className="my-0 md:my-1 text-sm md:text-base">
+                            {session.room} -{" "}
+                            {dayAndTimeFormatWithMonth.format(
+                                new Date(session.startTime),
+                            )}{" "}
+                            - {session.format}
+                        </p>
+                    </div>
+                )}
+
                 <p className="my-0 md:my-1 text-base md:text-lg font-bold">
                     {session.title} ({session.language})
                 </p>
+
+                <div className="text-sm md:text-base">
+                    {session.speakers.map((speaker) => (
+                        <p className="m-0" key={speaker.name}>
+                            {speaker.name}
+                        </p>
+                    ))}
+                </div>
+                <p className="text-lg text-end absolute bottom-0 right-1">
+                    {session.length} min
+                </p>
             </a>
-            <div className="text-sm md:text-base">
-                {session.speakers.map((speaker) => (
-                    <p className="m-0" key={speaker.name}>
-                        {speaker.name}
-                    </p>
-                ))}
-            </div>
-            <p className="text-lg text-end absolute bottom-0 right-1">
-                {session.length} min
-            </p>
+            <button
+                className="text-4xl hover:opacity-60 self-baseline"
+                data-session-id={`${session.id}`}
+                title={
+                    isFavorite ? "Remove from favorites" : "Add to favorites"
+                }
+                onClick={handleFavoriteClick}
+            >
+                {isFavorite ? "★" : "☆"}
+            </button>
         </div>
     );
 };
